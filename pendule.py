f7841b2948321fdfe4c98d5ada9b89d95763101a
Laurent Abbal <laurent@abbal.com>
2020-06-26 17:59:35 +0900

    Update pendule.py

--- pendule.py
+++ pendule.py
@@ -4,11 +4,12 @@ import math
 import digitalio
 from analogio import AnalogIn
 
-# ligne 6-9 : attenuer l'intensite de la diode de la carte
+
 import adafruit_dotstar
 ledin = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
 ledin.brightness = 0.01
 ledin[0] = (0, 255, 0)
+
 
 # LED
 led = digitalio.DigitalInOut(board.D0)
@@ -18,42 +19,79 @@ led.direction = digitalio.Direction.OUTPUT
 analog_in = AnalogIn(board.A3)
 n = 0
 ref = analog_in.value
-T = []
+
 last_clock = 0
 clock = 0
 cycle = 0
-while len(T) < 20:
+Ti = 0
+Tf = 0
+cycles = 0
+nb_periodes = 0
+nb_periodes_total = 10
+
+
+
+
+ref = 20000
+print(analog_in.value)
+
+while nb_periodes <= nb_periodes_total:
     led.value = False
-    n = n + 1
-    if n == 100:
-        ref = analog_in.value
-        n = 0
-    over = []
-    while analog_in.value > ref*1.2:
-        over.append(time.monotonic())
-        time.sleep(0.02)
-        print((analog_in.value,))
+    debut = 0
+    fin = 0
+    plot = ref
+    while analog_in.value > ref*1.5:
+        led.value = True
+        if debut == 0:
+            debut = time.monotonic()
+            print((plot*2,))
 
-    if over != []:
-        clock = over[len(over)//2]
-        cycle = cycle + 1
-
-    if cycle % 2 == 0:
-        if clock > last_clock:
-            if last_clock == 0:
-               last_clock = clock
-            else:
-                #T.append(clock - last_clock)
-                last_clock = clock
-                led.value = True
+       
+        #print((plot*2,))
+        #print((analog_in.value,))
+        #print(analog_in.value)
+        #time.sleep(0.04)
 
 
-    print(cycle)
-    print('T= ',T)
-    print((analog_in.value,))
+
+    fin = time.monotonic()
+
+    #print('debut= ',debut)
+    #print('fin= ',fin)
+
+    if debut != 0:
+
+
+        if cycles % 2 == 0:
+            nb_periodes = nb_periodes + 1
+            clock = debut + (fin-debut)/2
+            if Ti == 0:
+                Ti = clock
+
+            Tf = clock
+
+        cycles = cycles + 1
+        plot = plot*2
+
+
+
+
+    #print('--------------')
+    #print('Ref: ', ref)
+    #print('Time:', time.monotonic())
+    #print('Cycles: ', cycles)
+    #print('Ti= ',Ti)
+    #print('Tf= ',Tf)
+    #print('Clock= ',clock)
+    print((plot,))
+    #print((analog_in.value,))
+    
     time.sleep(0.02)
 
-T_av = sum(T) / len(T)
-l = (T_av**2 * 9.81) / (4*math.pi**2)
-print('Periode : ',T_av,'s')
+print(analog_in.value)
+T = (Tf - Ti) / nb_periodes_total
+l = (T**2 * 9.81) / (4*math.pi**2)
+print('Temps total: ', Tf - Ti)
+print('Periode : ',T,'s')
 print('Longueur du fil : ',l,'m')
+
